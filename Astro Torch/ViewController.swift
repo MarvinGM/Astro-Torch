1f9f05ebd34272b1f31ff55c429db7e11329f6eb
MarvinGM <marvinmahilum091197@gmail.com>
2021-05-28 12:07:10 +0800

    Version 1.0

--- Astro Torch/ViewController.swift
+++ Astro Torch/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var screenRed: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBAction func MyButtonClicked(_ sender: UIButton) {
+            
+        if(screenRed){
+            self.view.backgroundColor = UIColor.blue
+        } else {
+            self.view.backgroundColor = UIColor.red
+        }
+        screenRed = !screenRed
+    }
 }
 
